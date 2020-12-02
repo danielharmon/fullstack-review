a27f96d9326eb36516897976b1cadff8e97af2dc
danielharmon <dannyaharmon@gmail.com>
2020-12-02 12:13:10 -0800

    Refactor to use atlas mongoDB and env variables

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+const DBKEY = process.env.DBKEY
+mongoose.connect('DBKEY');
 
 let repoSchema = mongoose.Schema({
   _id: Number,
